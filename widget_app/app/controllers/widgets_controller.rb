fd7bcb867291638828b595932a6c3ef07383c228
Alexander Rowland <arowland900@gmail.com>
2019-01-18 13:50:33 -0800

    routes + bootstrap

---
+++ widget_app/app/controllers/widgets_controller.rb
@@ -0,0 +1,55 @@
+class WidgetsController < ApplicationController
+    before_action :set_widget, only: [:show, :edit, :update, :destroy]
+
+    def index
+        @widgets = Widget.all
+    end
+
+    def show
+        @widget = Widget.find(params[:id])
+    end
+
+    def new
+        @widget = Widget.new
+    end
+
+    def create
+        @widget = Widget.new(bean_params)
+      
+        if @widget.save
+            redirect_to widgets_path
+        else
+            render :new
+        end
+    end
+
+    def edit
+        @widget = Widget.find(params[:id])
+    end
+
+    def update
+        @widget = Widget.find(params[:id])
+
+        if @widget.update_attributes(widget_params)
+            redirect_to widgets_path
+        else
+            render :edit
+        end
+    end
+
+    def destroy
+        @widget = Widget.find(params[:id])
+        @widget.destroy
+        redirect_to widgets_path
+    end
+
+    private
+
+    def widget_params
+        params.require(:widget).permit(:description, :quantity)
+    end
+
+    def set_widget
+        @widget = Widget.find(params[:id])
+    end
+end
--- widget_app/config/routes.rb
+++ widget_app/config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
+  
+  root to: "widgets#index"
+
+  get "widgets/" => "widgets#index"
+	get "widgets/new" => "widgets#new", as: :new_widget
+	get "widgets/:id" => "widgets#show", as: :widget
+	post "widgets/" => "widgets#create"
+	get "widgets/:id/edit" => "widgets#edit", as: :edit_widget
+	patch "widgets/:id" => "widgets#update"
+	delete "widgets/:id" => "widgets#destroy"
+
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
